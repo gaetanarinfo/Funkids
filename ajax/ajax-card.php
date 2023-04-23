a04a6889d1369ed17cc4475db3d1dac754cbe990
gaetanarinfo <gaetanarinfo@gmail.com>
2023-04-23 13:55:26 +0200

    Push 23/04/2023

--- ajax/ajax-card.php
+++ ajax/ajax-card.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://unipay.center');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token');
 
